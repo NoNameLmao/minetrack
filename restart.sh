c2bf18c36881d82811de840685594eaa8014e2ab
NoNameLmao <egor5553049@gmail.com>
2023-02-08 13:41:58 +0300

    better restart script

--- restart.sh
+++ restart.sh
@@ -1,10 +1,31 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+BYellow='\033[1;33m'
+NC='\033[0m'
 # assuming you have pm2 installed!!!
-echo "Stopping minetrack..."
+quit() {
+    if [[ $1 -eq 1 ]]
+    then
+        echo -e "${BYellow}{RESTART} ${RED}Exiting due to error...${NC}"
+        exit 1
+    else
+        echo -e "${BYellow}{RESTART}${GREEN} Restart successful, exiting...${NC}"
+        exit $1
+    fi
+}
+echo -e "${BYellow}{RESTART} ${CYAN}Stopping minetrack...${NC}"
 pm2 stop minetrack
-echo "Pulling commits from github repo..."
+if [[ $? -eq 1 ]]
+    then
+        echo -e "${BYellow}{RESTART} ${CYAN}Recieved ${RED}exit code 1${CYAN}, either pm2 is not installed or it cannot find minetrack.${NC}"
+        quit 1
+    fi
+echo -e "${BYellow}{RESTART} ${CYAN}Pulling commits from github repo...${NC}"
 git pull
-echo "Resetting database..."
+echo -e "${BYellow}{RESTART} ${CYAN}Resetting database...${NC}"
 rm -rf database.sql database.sql-journal
-echo "Starting minetrack..."
+echo -e "${BYellow}{RESTART} ${CYAN}Starting minetrack...${NC}"
 pm2 start minetrack
-echo "done, might take a while to start up tho lol just be patient"
+quit 0
